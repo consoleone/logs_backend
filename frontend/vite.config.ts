e1288d292822a4f827388171e0497d6e1c1ba74a
Abhiram <anukoolaabhiram@outlook.com>
2023-02-14 14:39:29 +0530

    added base path

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
+const base = '/log/';
 export default defineConfig({
+  base,
   plugins: [react()],
-})
+});
